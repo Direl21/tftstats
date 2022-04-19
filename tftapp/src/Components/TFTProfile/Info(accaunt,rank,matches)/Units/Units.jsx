0b1c8449832babf2bed2706e651e7c23cbe84b7b
Direl21 <diana.tarabenko@gmail.com>
2022-04-19 20:10:56 +0200

    Create color border on units

--- tftapp/src/Components/TFTProfile/Info(accaunt,rank,matches)/Units/Units.jsx
+++ tftapp/src/Components/TFTProfile/Info(accaunt,rank,matches)/Units/Units.jsx
@@ -1,36 +1,40 @@
 import React from 'react';
+import Items from './Items/Items';
 import styleName from './Units.module.css';
 
 const Units = (units) => {
     let unitsList = units.units.map((champion, index) => {
+        console.log('units',units.units);
         let champions_names = champion.character_id; //champions names from API
         let champion_name_tooltip = `<span class="${styleName.tooltipName}">`+champions_names.split('_').pop() + `</span>`; //remove from string Set6_
-
+        //doble class and item icon for tooltip
         champion.items.map((item, index) => {
             champion_name_tooltip += `<span key=${index} class="${styleName.tooltipItemIcon}" >
                 <img alt='' src="/assets/items/${item}.png" />
             </span>`
-            })
+        });
+    
+        let border_color = "";
+        if (champion.rarity === 0) {
+            border_color = "grey"
+        } else if (champion.rarity === 1) {
+            border_color = "green"
+        } else if (champion.rarity === 2) {
+            border_color = "blue"
+        } else if (champion.rarity === 3) {
+            border_color = "pink"
+        } else if (champion.rarity === 4) {
+            border_color = "gold"
+        }
+
+
         return (
             <span className={styleName.champBox} key={index}>
-                <span data-tip={`${champion_name_tooltip}`} data-for='tooltipchampname' className={styleName.champIcon}>
+                <span data-tip={`${champion_name_tooltip}`} data-for='tooltipchampname' className={styleName.champIcon +' '+ styleName[border_color]}>
                     <img alt='' src={`../../../../assets/champions/champ-icon/${champions_names}.png`} />
                 </span>
                 <span className={styleName.itemBox}>
-
-                    {champion.itemNames.map((itemTooltip, index) => {
-                        let item_tooltip = itemTooltip.split('_').pop();
-                    })}
-
-                    {champion.items.map((item, index) => {
-                    console.log('item', item)
-                    return(
-                        <span key={index} data-for='tooltipitemname' className={styleName.itemIcon} >
-                            <img alt='' src={`../../../../assets/items/${item}.png`} />
-                        </span>
-                    )
-                    })}
-                    
+                    <Items itemNum={champion.items} />
                 </span>
             </span>
         )
